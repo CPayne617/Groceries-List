280138c2a310dda36faa1008caf7bd9c8f391781
C.Payne <cpayne617@gmail.com>
2021-09-05 19:28:12 -0400

    add two ReadMe files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    list: ['juices', 'beef', 'cookies']
+  }
+
+  updatedList = () => {
+    this.setState({
+      list: ['water', ...this.state.list]
+    })
+  }
+  removeItem = (index) => {
+    this.setState({
+      list: this.state.list.filter((_, i) => i !== index)
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div>
+          {
+            this.state.list.map((item) => {
+              return (
+                <h1>{item}</h1>
+              )
+            })
+          }
+        </div>
+        <button onClick={this.updatedList}>Add item</button>
+        <button onClick={() => this.removeItem(0)}>Remove items</button>
+      </div>
+    )
+  }
 }
-
 export default App;
